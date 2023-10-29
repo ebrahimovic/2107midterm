75da127c8fe8ab73cf94d16974488c9074763583
Ali Al Ibrahim <the.ali.alibrahim@gmail.com>
2023-10-28 18:01:51 -0700

    final draft, couldn't advance more due to the limit on the times I can access the api per day

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,34 @@ import { Card, Row, Col, Form, FormControl, Button } from 'react-bootstrap';
 import './App.css';
 
 const Key = 'a1970357721e47f6941c4fc09d42af48';
-const URL = `https://newsapi.org/v2/everything?q=apple&sortBy=popularity&apiKey=${Key}`;
+const publishedAt = "publishedAt";
+const popularity = "popularity";
+const relevancy = "relevancy";
+const initialPage = 1; 
+const cardsPerPage = 24; 
+let totalArticles = 0;
+let totalPages = 1;
+let URL = `https://newsapi.org/v2/everything?&apiKey=${Key}&page=${initialPage}`;
 
-// Array of border colors
+
 const borderColors = ['primary', 'secondary', 'success', 'danger', 'warning', 'info', 'dark'];
 
-function NewsCard({ title, text, imgSrc, url, index }) {
-  const borderColor = borderColors[index % borderColors.length]; // Use modulo to cycle through colors
+function NewsCard({ title, text, imgSrc, url, publishedDate, index }) {
+  const borderColor = borderColors[index % borderColors.length]; 
+
+
+  const formattedDate = new Date(publishedDate).toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
+
   return (
     <Card border={borderColor}>
-      <Card.Img variant="top" src={imgSrc} style={{ width: '100%', height: '200px' }} />
+      <div className="image-container">
+        <Card.Img variant="top" src={imgSrc} style={{ width: '100%', height: '200px' }} />
+        <div className="published-date">{formattedDate}</div>
+      </div>
       <Card.Body>
         <Card.Title>{title}</Card.Title>
         <Card.Text>{text}</Card.Text>
@@ -28,30 +46,69 @@ function HomePage(props) {
   const [newsData, setNewsData] = useState([]);
   const [error, setError] = useState(null);
   const [searchTerm, setSearchTerm] = useState('');
-
-  useEffect(() => {
-    const getNewsData = async () => {
-      try {
-        const response = await fetch(URL);
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const data = await response.json();
-        setNewsData(data.articles);
-      } catch (error) {
-        setError(error.message);
-      }
-    };
-    getNewsData();
-  }, []);
+  const [currentPage, setCurrentPage] = useState(initialPage);
+  const [sortOption, setSortOption] = useState(publishedAt); 
 
   const handleSearchChange = (event) => {
     setSearchTerm(event.target.value);
   };
 
-  const filteredNews = newsData.filter((newsItem) =>
-    newsItem.title.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+  const handleSortBy = (sort) => {
+    setSortOption(sort);
+    setCurrentPage(initialPage); 
+  };
+
+  useEffect(() => {
+    const getNewsData = async () => {
+      try {
+        const updatedURL = `https://newsapi.org/v2/everything?q=apple&sortBy=${sortOption}&apiKey=${Key}&page=${currentPage}`;
+        const response = await fetch(updatedURL);
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        setNewsData(data.articles);
+        totalArticles = data.articles.length;
+        totalPages = Math.ceil(totalArticles / cardsPerPage);
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+    getNewsData();
+  }, [sortOption, currentPage]);
+
+
+  const updateURL = () => {
+    URL = `https://newsapi.org/v2/everything?q=apple&sortBy=${publishedAt}&apiKey=${Key}&page=${currentPage}`;
+  };
+
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+
+  const startIndex = (currentPage - 1) * cardsPerPage;
+  const endIndex = Math.min(startIndex + cardsPerPage, totalArticles);
+  const filteredNews = newsData
+    .filter((newsItem) => newsItem && newsItem.title && newsItem.title.toLowerCase().includes(searchTerm.toLowerCase()))
+    .slice(startIndex, endIndex);
+
+
+  useEffect(() => {
+    updateURL();
+  }, [currentPage]);
+
+
 
   return (
     <div>
@@ -68,6 +125,23 @@ function HomePage(props) {
           </Form>
         </div>
       </div>
+      <div className='sortNews'>
+        <div>
+          <Button variant="primary" onClick={() => handleSortBy("publishedAt")}>
+            Latest
+          </Button>
+        </div>
+        <div>
+          <Button variant="primary" onClick={() => handleSortBy("popularity")}>
+            Popularity
+          </Button>
+        </div>
+        <div>
+          <Button  onClick={() => handleSortBy("relevancy")}>
+            Relevancy
+          </Button>
+        </div>
+      </div>
       <div className="myContainer">
         {filteredNews.length === 0 ? (
           <div className="notFound">
@@ -82,12 +156,22 @@ function HomePage(props) {
                   text={newsItem.description}
                   imgSrc={newsItem.urlToImage}
                   url={newsItem.url}
-                  index={index} // Pass the index to NewsCard
+                  publishedDate={newsItem.publishedAt} 
+                  index={index}
                 />
               </Col>
             ))}
           </Row>
         )}
+        <div className="pagination">
+          <Button variant="primary" onClick={handlePrevPage} disabled={currentPage === 1}>
+            Back
+          </Button>
+          <span>Page {currentPage} of {totalPages}</span>
+          <Button variant="primary" onClick={handleNextPage} disabled={currentPage === totalPages}>
+            Next
+          </Button>
+        </div>
       </div>
     </div>
   );
